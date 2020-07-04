63c90320456c840c57eedac3e6cab79fb0769213
Adrien Carré <ac@jandk.fr>
2020-07-04 17:34:45 +0200

    Handling the case of a corrected regression

--- src/regression.js
+++ src/regression.js
@@ -53,6 +53,13 @@ function lookForRegressions (evolutionArray) {
 
 				wasPassingBefore = true;
 
+				// If we already added it to the regression array, this means we added it from an old historic so we need to remove if
+				let addedIndex = testsAdded.indexOf(testInfo.test.id);
+				if (addedIndex > -1) {
+					testsThatHasRegressed.splice(addedIndex, 1);
+					testsAdded.splice(addedIndex, 1);
+				}
+
 			}
 
 		}
